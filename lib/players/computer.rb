528797bc3d7dd269b5b83ee220514f5ef8368586
Christopher Wood <cjwood032@gmail.com>
2018-06-20 13:55:55 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -15,7 +15,7 @@ class Computer < Player
 
 #if comp has middle and opponent chooses a side
 #doesn't work yet
-    elsif board.turn_count==3 && (board.taken?(2) || board.taken(4) || board.taken(6) || board.taken(8))
+    elsif board.turn_count==3 && (board.taken?(2) || board.taken?(4) || board.taken?(6) || board.taken?(8))
       if board.taken?(2) || board.taken?(4)
         move="1"
       else
